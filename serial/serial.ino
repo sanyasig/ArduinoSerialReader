d2aa0167c44bf9c5f7c25ea8148bc7b44ffbcbb9
sanyasi_g <nani76.gsrao@gmail.com>
2015-02-14 20:58:29 +0000

    working json parser

--- serial/serial.ino
+++ serial/serial.ino
@@ -6,9 +6,12 @@ STEING values and all the values are  Number values
 boolean toggleComplete = false;
 boolean pwmComplete = false;
 String inputString;
-char endChar = 'e';
-char beginChar = 'b';
-char newChar;
+char endChar = 'z';
+char beginChar = 'q';
+String value;
+String lable;
+
+
 void setup()
 {
 	Serial.begin(9600);
@@ -17,20 +20,55 @@ void setup()
 void  loop()
 {
 	while(Serial.available()){
-		newChar = (char)Serial.read();
+		char newChar = (char)Serial.read();
 		if (newChar == beginChar){
+			value  = "";
+			lable = "";
 			inputString = "";
 		}
 		else if (newChar == endChar){
 			toggleComplete = true;
-			}else{
-				inputString += newChar; 
+			}
+                else{
+           		inputString += newChar; 
 			}
 		}
 
 	if(toggleComplete == true){
-		Serial.println(inputString);
-		toggleComplete = false;
+               	getCommand(inputString);
+		Serial.println("lable: " + lable);
+		Serial.println("value: " + value);
+		toggleComplete = false; 
 	}
 
 }
+void getCommand(String stringValue){ 
+	boolean isLable = false;
+	boolean isValue = false;
+	boolean end = false;
+	for (int i=0; i<stringValue.length(); i++){
+		char eachChar = stringValue.charAt(i);
+
+		if((char)eachChar == '}'){
+			break;
+		}
+		if(eachChar == '{'){
+			isLable = true;
+			isValue = false;
+		}
+		if (eachChar == ':'){
+			isLable = false;
+			isValue = true;
+		}
+
+		if(isLable && eachChar != '{'){
+			lable += eachChar;
+		}
+		if(isValue && eachChar != ':'){
+			value += eachChar;
+		}
+
+	}
+}
+
+
